448fbd435350fa490db4ebad4eef271b784b4294
Olesia <oles.ivanenko@gmail.com>
2017-02-10 01:24:04 +0200

    fixed loop

--- fight.js
+++ fight.js
@@ -29,47 +29,42 @@ Animal.prototype.fight = function (enemy) {
 // here should be all my computing
 Animal.prototype.resist = function(hit_power) {
 
-	console.log(this.nameAnimal + "["+this.hp+"/100] got hit on " + (hit_power- this.resistance) + " points. hp left [" + (this.hp -= hit_power - this.resistance) +"/100]" );
-
+	 console.log(this.nameAnimal + "["+this.hp+"/100] got hit on " + (hit_power- this.resistance) + " points. hp left [" + (this.hp -= hit_power - this.resistance) +"/100]" );
+	 
 	 //console.log("hp left [" + this.hp +"/100]");
-	}
+}
 
-	function Snake() {
-		Animal.apply(this,arguments);
-	}
-	Snake.prototype = Object.create(Animal.prototype);
-	Snake.prototype.constructor = Animal;
+function Snake() {
+	Animal.apply(this,arguments);
+}
+Snake.prototype = Object.create(Animal.prototype);
+Snake.prototype.constructor = Animal;
+ 
+
+ function Ratatu() {
+	Animal.apply(this,arguments);
+}
+Ratatu.prototype = Object.create(Animal.prototype);
+Ratatu.prototype.constructor = Animal;
 
 
-	function Ratatu() {
-		Animal.apply(this,arguments);
-	}
-	Ratatu.prototype = Object.create(Animal.prototype);
-	Ratatu.prototype.constructor = Animal;
 
-
-
-	function randInterval (min, max) {
-		rand = Math.round(Math.random()*(max-min)+min);
-		return rand;
-	}
+function randInterval (min, max) {
+	rand = Math.round(Math.random()*(max-min)+min);
+	return rand;
+}
 
 //new Snake(100, "egor", 78, 40).fight();
 var snake = new Snake(100, "Snake", 20, 4);
 var rat = new Ratatu(100, "Cookrat", 10, 4);
 
-while (true) {
+for (var i=0; i<20; i++) {
 
-	if (rat.isAlive() && snake.isAlive()) {
+	if (rat.isAlive() == true || snake.isAlive() == true) {
 		rat.fight(snake);
 		snake.fight(rat);
-	} else if (rat.isAlive()) {
-		snake.fight(rat);
-	} else if (snake.isAlive()) {
-		rat.fight(snake);
-	} else break;
-
-} console.log("everybody dont want to fight") ;
+	}	else console.log("everybody dont want to fight");
+}
 
 
 
@@ -77,7 +72,7 @@ while (true) {
 // var timer = setInterval(function() {
 // 	//console.log("step");
 // }, 1000);
-
+ 
 
 //setTimeout(function() {
 //clearInterval(timer);
