e891eba9c2e681e963faf394ed16d680ea0898cf
olko <oles.ivanenkp@gmail.com>
2017-10-22 23:59:30 +0300

    Added anagramma sorting anagramma,  unique word sorting

---
+++ learn.javascript.ru/array-methods/task_15.js
@@ -0,0 +1,22 @@
+var arr = ["воз", "киборг", "корсет", "ЗОВ", "гробик", "костер", "сектор"];
+var obj = {}
+var result = [];
+
+function aclean(arr) {
+
+    for(var i=0; i<arr.length; i++) {
+        var sorted = arr[i].toLowerCase().split('').sort().join("");
+
+        var key = sorted;
+        obj[key] = arr[i];
+    }
+
+    for(key in obj) {
+        result.push(obj[key]);
+    }
+    return result;
+}
+
+console.log(aclean(arr)); // "воз,киборг,корсет" или "ЗОВ,гробик,сектор"
+
+process.exit(1);
