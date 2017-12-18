3028be64a03768853641299d3c831e91f462e98b
olko <oles.ivanenko@gmail.com>
2017-12-18 09:52:20 +0200

    Gotted how many src to tomorrow

--- learn.javascript.ru/datetime/task_6.js
+++ learn.javascript.ru/datetime/task_6.js
@@ -1,11 +1,11 @@
 
 function getSecondsToday() {
     var now = new Date();
-    
+
     var today = new Date();
     today.setHours(0,0,0,0);
     
-    return (now - today)/1000;
+    return Math.floor((now - today)/1000);
 }
 
 // alternative solving //
@@ -15,6 +15,17 @@ function getSecondsToday() {
 //     return d.getHours() * 3600 + d.getMinutes() * 60 + d.getSeconds();
 //   };
 
-console.log(getSecondsToday()); // (3600 * 10)
 
+
+
+function getSecToTomorrow() {
+    var now = new Date();
+    var tomorrow = new Date();
+    tomorrow.setDate(now.getDate()+1);
+    console.log(tomorrow);
+    return Math.floor((tomorrow - now)/1000);
+
+}
+console.log(getSecToTomorrow());
+// console.log("Seconds have passed today is " + getSecondsToday()); // (3600 * 10)
 process.exit(1);
