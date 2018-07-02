11afb37dd2a64bcb04b8220c46348b3edad650f7
Anton <anton12006@yandex.ru>
2018-07-02 12:32:47 +0300

    Update wrapObjectCreate.js

--- wrapObjectCreate.js
+++ wrapObjectCreate.js
@@ -1,22 +1,28 @@
+// Example of wrapping some function 
 var a = Object.create(null);
-a.hasOwn.....
+// a.hasOwn.....
 
------------------------
+// -----------------------
 
 var oldCreate = Object.create;
 
-Object.create = function(...args) {
+Object.create2 = function(...args) {
 	var res = oldCreate(args);
-	console.log("asd"+args)
+	console.log("create2 invoked");
 	res.myField = 1;
-	res.func = function(a){
-		console.log(a);
+	res.someFunc = function(){
+		console.log("someFunc");
 	}
 	return res;
 	
 }
 
 var o = Object.create(null);
-
-
 var o2 = Object.create2(null);
+
+// o.someFunc(); -- not exist
+
+
+o2.someFunc();
+console.log("o2.myField = " + o2.myField);
+
